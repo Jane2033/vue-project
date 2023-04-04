670ff8b4efae05ad0837ec578cb2d175be92e746
張雅蓁 <zhangyazhen@zhangyazhendeMacBook-Pro.local>
2023-04-04 16:14:26 +0800

    add spline

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex flex-col min-h-screen bg-gradient-to-br from-pink700 via-purple400 to-red200 font-NotoSans">
     <Navigation />
+    <Hero />
     <RouterView />
   </div>
 </template>
@@ -8,8 +9,10 @@
 <script setup>
   import {RouterView} from "vue-router";
   import Navigation from "./components/Navigation.vue";
+  import Hero from "./components/Hero.vue";
 </script>
 
 <style lang="scss" scoped>
 
 </style>
+
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,5 +5,61 @@
 </template>
 
 <script setup>
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import SplineLoader from '@splinetool/loader';
+
+// camera
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 5, 100000);
+camera.position.set(-125.44, -24.07, 1020);
+camera.quaternion.setFromEuler(new THREE.Euler(0, -0.13, 0));
+
+// scene
+const scene = new THREE.Scene();
+
+// spline scene
+const loader = new SplineLoader();
+loader.load(
+  'https://prod.spline.design/Avcc73xqeEtr2uOR/scene.splinecode',
+  (splineScene) => {
+    scene.add(splineScene);
+  },
+  null,
+	// called when loading has errors
+	(error) => {
+		console.log('An error happened');
+	}
+);
+
+// renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+// scene settings
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
+
+scene.background = new THREE.Color('#121316');
+renderer.setClearAlpha(0);
+
+// orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.125;
+
+window.addEventListener('resize', onWindowResize);
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate(time) {
+  controls.update();
+  renderer.render(scene, camera);
+}
+
 
 </script>
